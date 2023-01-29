a0a11d31ba2298db4cdd52895e55fa047a99db5a
chamika-prasad <chamikap40@gmail.com>
2023-01-29 14:01:21 +0530

    final_commit_backend

---
+++ Assignment/Request/CartRequest.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Assignment.Request
+{
+    public class CartRequest
+    {
+        [Required]
+        public List<int> cartIds { get; set; }
+    }
+}
